787a0d8e6cbf6cc9f10a508c9b5f8d5179e9148c
PhyscoKillerMonkey <reece.bennett1@gmail.com>
2015-03-22 22:15:04 +0000

    Cleanup and added debug stuffs

--- 31.js
+++ 31.js
@@ -7,15 +7,22 @@ var animate = window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     function (callback) {window.setTimeout(callback, 1000 / 60);};
 
+// Menu stuffs
+var debugMenu = document.getElementById("debug");
+
 function play31() {
+  
+  var debug = false; // Enable degug
+  if (!debug) debugMenu.style.display = "none";
+  
   var canvas = document.getElementById("31"),
     ctx = canvas.getContext("2d"),
     now,
     dt,
     last = window.performance.now,
-    step = 1 / 60, // Try to update game 60 times a second
-    cSize = 8, // Size of cell in pixels
-    gridSize = 31; // Size of board in "pixels" (number of cells) STARTS AT 1,1 in top left
+    step = 1 / 60,  // Try to update game 60 times a second
+    cSize = 8,  // Size of cell in pixels
+    gridSize = 31;  // Size of board in "pixels" (number of cells) STARTS AT 1,1 in top left
       
   function render() {
     
@@ -32,19 +39,24 @@ function play31() {
   }
 
   function gameLoop() {
-    now = window.performance.now;
-    dt = Math.min(1, (now - last) / 1000);    // duration in seconds
+    
+    now = window.performance.now();
+    dt = Math.min(1, (now - last) / 1000);  // duration in seconds
+    
     while (dt > step) {
-      dt = dt - step;
+      dt -= step;
       update(step);
     }
+    
+    if (debug) debugMenu.innerHTML = "FrameTime: " + now.toFixed();
+    
     render(dt);
     last = now;
 
-    animate(update);
+    animate(gameLoop);
   }
   
-  animate(gameLoop);
+  gameLoop();
   
 }
 
