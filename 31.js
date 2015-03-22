75a76f2a0d4fadf75b46a1e34df28fed554727da
John Evans <burntcustard@gmail.com>
2015-03-22 21:14:26 +0000

    Initial commit

---
+++ 31.js
@@ -0,0 +1,44 @@
+/*jslint plusplus: true, browser: true, devel: true*/
+function play31() {
+  "use strict";
+  var canvas = document.getElementById("31"),
+    ctx = canvas.getContext("2d"),
+    now,
+    dt,
+    last = window.performance.now,
+    step = 1 / 60, // Try to update game 60 times a second
+    cSize = 8, // Size of cell in pixels
+    gridSize = 31; // Size of board in "pixels" (number of cells) STARTS AT 1,1 in top left
+      
+  function render() {
+    
+    function paintCell(color, x, y) {
+      ctx.fillRect(x * cSize, y * cSize, cSize, cSize);
+    }
+    
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+  }
+
+  function update() {
+  }
+
+  function gameLoop() {
+    now = window.performance.now;
+    dt = Math.min(1, (now - last) / 1000);    // duration in seconds
+    while (dt > step) {
+      dt = dt - step;
+      update(step);
+    }
+    render(dt);
+    last = now;
+
+    window.requestAnimationFrame(update);
+  }
+  
+  window.requestAnimationFrame(gameLoop);
+  
+}
+
+window.onload = function () { play31(); };
