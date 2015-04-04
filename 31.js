424ac604d4bd44192327b88a275d6fbef1fffab2
John <burntcustard@gmail.com>
2015-04-04 20:27:44 +0100

    Not sure what I've just done/not done

--- 31.js
+++ 31.js
@@ -1,5 +1,5 @@
 /*jslint plusplus: true, browser: true, devel: true, node: true, unparam: true, vars: true, white: true*/
-/*global FPSMeter*/
+/*global FPSMeter, newGame*/
 "use strict";
 
 var canvas = document.getElementById("canvas31"),
@@ -341,7 +341,7 @@ function Emitter(options) {
       //if (debug) { console.log(e); }
       //if (debug) { console.log(spawnInto); }
       spawnInto.entities.push(e);
-      spawnInto.collidable.push(e);
+      if (this.emitCollidable !== "no") { spawnInto.collidable.push(e); }
       this.lastEmission = now;
     }
   };
@@ -362,14 +362,14 @@ function Ship(options) {
     this.y = gridSizeY - this.sprite.h - 2;
     this.facing = 'U';
   }
-  
+
   // Initial position for enemy ship:
   if (this.name === "enemy" && !this.x && !this.y) { // !this.x == true if this.x == 0;
     this.x = Math.round(gridSizeX / 2) - Math.round(this.sprite.w / 2);
     this.y = 0;
     this.facing = 'D';
   }
-  
+
   // Colour stuff
   this.primaryColor = options.primaryColor || "rgba(0,0,0,0)";
   this.secondaryColor = options.secondaryColor || "rgba(0,0,0,0)";
@@ -393,7 +393,7 @@ function Ship(options) {
       if (this.weapons[i].type) { this.weapons[i].type.emit(level); }
     }
   };
-  
+
   // Initial draw creates the object off screen, then these two images both get
   // drawn onto the main canvas when this.draw() is called. Each entity that is coloured
   // in this way needs to have it's own canvas or two (I think), so we should come up with a way
@@ -428,7 +428,7 @@ function Ship(options) {
     
     // Draw weapons
     for (i = 0; i < this.weapons.length; i++) {
-      if (this.weapons[i].type) { // If there is a weapon in this weapon slot      
+      if (this.weapons[i].type) { // If there is a weapon in this weapon slot
         if (this.sprite.index === 0) { tilt = this.weapons[i].tiltOffsetL; } else
         if (this.sprite.index === 2) { tilt = this.weapons[i].tiltOffsetR; }
 
@@ -549,7 +549,7 @@ function Bullet(options) {
     w: 1, h: 2
   };
   this.maxVelocity = 1.5;
-  this.explosion = SmallExplosion; 
+  this.explosion = SmallExplosion;
   Entity.call(this, options);
 }
 function SmallGun(options) {
@@ -672,7 +672,7 @@ function play31() {
     ctx.imageSmoothingEnabled = false;
 
     // Fill canvas with levels color
-    ctx.fillStyle = "#2b383b";
+    ctx.fillStyle = "#2c2c39";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw stuff in entity list
@@ -749,7 +749,9 @@ function play31() {
       if (ent.y < -ent.sprite.h || ent.y > gridSizeY + ent.sprite.h ||
           ent.x < -ent.sprite.w || ent.x > gridSizeX + ent.sprite.w) {
         level.entities.splice(i, 1);
-        level.collidable.splice(level.collidable.indexOf(ent), 1);
+        if (level.collidable.indexOf(ent) !== -1) {
+          level.collidable.splice(level.collidable.indexOf(ent), 1);
+        }
       }
     }
 
@@ -851,9 +853,9 @@ function play31() {
     if (meter) { meter.tick(); }  // FPS Meter tick finish
 
     last = now;
-    
+
     // Switching to new levels hmm this is messy
-    var loop = true; // Are you gonn' keep looping? 
+    var loop = true; // Are you gonn' keep looping?
     if (keys.one) {
       loop = false;
       keys.one = false;
@@ -887,7 +889,7 @@ function play31() {
         id: ''
       };
     enemyShip = false;
-    
+
     // Create new level
     switch (levelID) {
     case "level1":
@@ -903,11 +905,51 @@ function play31() {
       var rockSpawner = new Emitter({
         x: 0, y: -4,
         emitX: [0, gridSizeX - 4],
-        ammo: new MediumRock({}),
+        ammo: new MediumRock({
+          maxVelocity: 1
+        }),
         spawnInto: level,
-        cooldown: 1000
+        cooldown: 500
       });
       level.emitters.push(rockSpawner);
+
+      var starSpawner = new Emitter({
+        y: -1,
+        emitX: [0, gridSizeX],
+        ammo: {
+          name: "star",
+          maxVelocity: 0.5,
+          sprite: {w: 1, h: 1},
+          draw: function(context) {
+            paintCell(context, this.x, this.y, "rgba(255, 255, 255, 0.2)");
+            paintCell(context, this.x, this.y - 1, "rgba(255, 255, 255, 0.15)");
+            paintCell(context, this.x, this.y - 2, "rgba(255, 255, 255, 0.1)");
+            paintCell(context, this.x, this.y - 3, "rgba(255, 255, 255, 0.05)");
+          }
+        },
+        spawnInto: level,
+        cooldown: 400
+      });
+      starSpawner.emitCollidable = "no";
+      level.emitters.push(starSpawner);
+
+      var starSpawner_2 = new Emitter({
+        y: -1,
+        emitX: [0, gridSizeX],
+        ammo: {
+          name: "star",
+          maxVelocity: 0.25,
+          sprite: {w: 1, h: 1},
+          draw: function(context) {
+            paintCell(context, this.x, this.y, "rgba(255, 255, 255, 0.1)");
+            paintCell(context, this.x, this.y - 1, "rgba(255, 255, 255, 0.05)");
+          }
+        },
+        spawnInto: level,
+        cooldown: 200
+      });
+      starSpawner_2.emitCollidable = "no";
+      level.emitters.push(starSpawner_2);
 
       break;
 
@@ -925,13 +967,13 @@ function play31() {
         name: "enemy"
       });
       level.collidable.push(enemyShip);
-        
+
       break;
-        
+
     default:
       throw new Error ("Tried to load unknown level.");
     }
-    
+
     gameLoop();
   }
 
